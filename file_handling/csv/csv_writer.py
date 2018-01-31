41e9720b57a1f7c1edae0bb038dee321eaeba390
Avery Uslaner <uslaner.avery@gmail.com>
2018-01-31 16:34:00 -0700

    Added CSV dictionary writer function
    Added a function to the csv_writer script that writes a csv file from data organized as a dictionary.

--- file_handling/csv/csv_writer.py
+++ file_handling/csv/csv_writer.py
@@ -1,12 +1,27 @@
 import csv
 
 
+def csv_dict_writer(data, path, fieldnames):
+    """
+    Writes a CSV file using DictWriter.
+    :param data: Data to be written to output CSV file.
+    :param path: File path to save output CSV file.
+    :param fieldnames: Defines column/fieldnames.
+    :return: None.
+    """
+    with open(path, 'w', newline='') as out_file:
+        writer = csv.DictWriter(out_file, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in data:
+            writer.writerow(row)
+
+
 def csv_writer(data, path):
     """
     Write data to a CSV file path.
     :param data: Data to be written as output CSV file.
     :param path: File path to save output CSV file.
-    :return: Returns nothing.
+    :return: None.
     """
     with open(path, "w", newline='') as csv_file:
         writer = csv.writer(csv_file)
@@ -20,5 +35,12 @@ if __name__ == '__main__':
             "Jules,Dicki,Lake Nickolasville".split(","),
             "Dedric,Medhurst,Stiedemannberg".split(",")
             ]
+    my_list = []
+    fieldnames = data[0]
+    for values in data[1:]:
+        inner_dict = dict(zip(fieldnames, values))
+        my_list.append(inner_dict)
     path = "output.csv"
+    dict_path = "dict_output.csv"
     csv_writer(data, path)
+    csv_dict_writer(my_list, dict_path, fieldnames, )
