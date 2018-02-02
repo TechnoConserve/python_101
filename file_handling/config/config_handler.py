f80b23322bdf3c094b73c57f07509c0e30234759
Avery Uslaner <uslaner.avery@gmail.com>
2018-02-01 17:03:45 -0700

    Configuration Script with CRUD
    Added function that demonstrates various CRUD operations in basic configuration script.

--- file_handling/config/config_handler.py
+++ file_handling/config/config_handler.py
@@ -1,4 +1,5 @@
 import configparser
+import os
 
 
 def createConfig(path):
@@ -18,6 +19,33 @@ def createConfig(path):
         config.write(config_file)
 
 
+def crudConfig(path):
+    """
+    Create, read, update, delete configuration options.
+    :param path: File path to save output configuration file.
+    :return: None
+    """
+    if not os.path.exists(path):
+        createConfig(path)
+
+    config = configparser.ConfigParser()
+    config.read(path)
+
+    # Read some values from the config
+    font = config.get("Settings", "font")
+    font_size = config.get("Settings", "font_size")
+
+    # Change a value in the config
+    config.set("Settings", "font_size", "12")
+
+    # Delete a value from the config
+    config.remove_option("Settings", "font_style")
+
+    # Write changes back to the config file
+    with open(path, "w") as config_file:
+        config.write(config_file)
+
+
 if __name__ == "__main__":
     path = "settings.ini"
-    createConfig(path)
+    crudConfig(path)
