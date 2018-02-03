5be3087d056412fbeeb7752e5e08035996000990
Avery Uslaner <uslaner.avery@gmail.com>
2018-02-02 19:30:20 -0700

    Added very simple script that demonstrates logging using the python standard library.
    Signed-off-by: Avery Uslaner <uslaner.avery@gmail.com>

---
+++ logging/simple_logger.py
@@ -0,0 +1,9 @@
+import logging
+
+
+# Add filemode="w" to overwrite
+logging.basicConfig(filename="sample.log", level=logging.INFO)
+
+logging.debug("This is a debug message")
+logging.info("Informational message")
+logging.error("An error has happened!")
