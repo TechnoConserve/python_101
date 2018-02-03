5844114a39d03cb52e300ec922efd2bb05d70421
Avery Uslaner <uslaner.avery@gmail.com>
2018-02-02 19:12:23 -0700

    Configuration Script CRUD refactoring
    Refactored existing CRUD functionality into their own functions, renamed functions to comply with PEP8 styling, and added remaining CRUD functionality.

--- file_handling/config/config_handler.py
+++ file_handling/config/config_handler.py
@@ -2,7 +2,7 @@ import configparser
 import os
 
 
-def createConfig(path):
+def create_config(path):
     """
     Create a configuration file.
     :param path: File path to save output configuration file.
@@ -19,17 +19,13 @@ def createConfig(path):
         config.write(config_file)
 
 
-def crudConfig(path):
+def crud_config(path):
     """
     Create, read, update, delete configuration options.
     :param path: File path to save output configuration file.
     :return: None
     """
-    if not os.path.exists(path):
-        createConfig(path)
-
-    config = configparser.ConfigParser()
-    config.read(path)
+    config = get_config(path)
 
     # Read some values from the config
     font = config.get("Settings", "font")
@@ -46,6 +42,71 @@ def crudConfig(path):
         config.write(config_file)
 
 
+def delete_setting(path, section, setting):
+    """
+    Delete a setting.
+    :param path: File path to search for the configuration file.
+    :param section: Name of the section heading in the config file.
+    :param setting: Name of the setting to delete.
+    :return: None
+    """
+    config = get_config(path)
+    config.remove_option(section, setting)
+    with open(path, "w") as config_file:
+        config.write(config_file)
+
+
+def get_config(path):
+    """
+    Returns the config object.
+    :param path: File path to search for the configuration file.
+    :return: File object of the configuration file.
+    """
+    if not os.path.exists(path):
+        create_config(path)
+
+    config = configparser.ConfigParser()
+    config.read(path)
+    return config
+
+
+def get_setting(path, section, setting):
+    """
+    Print out a setting.
+    :param path: File path to search for the configuration file.
+    :param section: Name of the section heading in the config file.
+    :param setting: Name of the setting to check.
+    :return: Value of the given setting.
+    """
+    config = get_config(path)
+    value = config.get(section, setting)
+    msg = "{section} {setting} is {value}".format(
+        section=section, setting=setting, value=value
+    )
+    print(msg)
+    return value
+
+
+def update_setting(path, section, setting, value):
+    """
+    Update a setting.
+    :param path: File path to search for the configuration file.
+    :param section: Name of the section heading in the config file.
+    :param setting: Name of the setting to update.
+    :param value: Value to change the setting to.
+    :return: None
+    """
+    config = get_config(path)
+    config.set(section, setting, value)
+    with open(path, "w") as config_file:
+        config.write(config_file)
+
+
 if __name__ == "__main__":
     path = "settings.ini"
-    crudConfig(path)
+    font = get_setting(path, "Settings", "font")
+    font_size = get_setting(path, "Settings", "font_size")
+
+    update_setting(path, "Settings", "font_size", "12")
+
+    delete_setting(path, "Settings", "font_style")
