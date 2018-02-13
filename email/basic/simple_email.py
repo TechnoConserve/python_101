66d5f6b4c22e21566a06181b6d6b31e34ace4861
Avery Uslaner <uslaner.avery@gmail.com>
2018-02-13 15:13:00 -0700

    Refactored Email with Gmail SMTP Server
    Refactored script to make functionality a bit more extensible and useful. Separated email sending and server authentication into their own functions.
    
    Signed-off-by: Avery Uslaner <uslaner.avery@gmail.com>

--- email/basic/simple_email.py
+++ email/basic/simple_email.py
@@ -1,34 +1,51 @@
 import smtplib
+import sys
 
 from email.message import EmailMessage
 from email.headerregistry import Address
 
 from config.config_handler import get_setting
 
-USERNAME = get_setting('config/settings.ini', 'Auth', 'username')
-PASSWORD = get_setting('config/settings.ini', 'Auth', 'password')
+CONFIG_FILE = 'config/settings.ini'
+HOST = get_setting(CONFIG_FILE, 'Auth', 'host')
+USERNAME = get_setting(CONFIG_FILE, 'Auth', 'username')
+PASSWORD = get_setting(CONFIG_FILE, 'Auth', 'password')
 
-HOST = "smtp.gmail.com"
-SUBJECT = "Test email from Python"
-TO = "admin@averyuslaner.com"
-FROM = "uslaner.avery@gmail.com"
-text = "This is an email message."
 
-msg = EmailMessage()
-msg['Subject'] = SUBJECT
-msg['From'] = Address('Avery Uslaner', addr_spec=FROM)
-msg['To'] = Address('Avery Uslaner', addr_spec=TO)
-msg.set_content("""\
-Hello!
+def authenticate(host, username, passwd):
+    """
+    Authenticated with SMTP server and return server object.
+    :return: Authenticated server object.
+    """
+    server = smtplib.SMTP_SSL(host)
+    server.set_debuglevel(1)
+    server.connect(host)
+    server.login(username, passwd)
+    return server
 
-This is an email sent from a python script.
 
--Avery Uslaner
-""")
+def send_email(subject, body_text, emails):
+    """
+    Send an email then close the server connection.
+    :param subject: Subject header.
+    :param body_text: Body of the message.
+    :param emails: Email addresses to send to. E.g. person@domain.com
+    :return: None
+    """
+    server = authenticate(HOST, USERNAME, PASSWORD)
 
-server = smtplib.SMTP_SSL(HOST)
-server.set_debuglevel(1)
-server.connect(HOST)
-server.login(USERNAME, PASSWORD)
-server.send_message(msg)
-server.quit()
+    msg = EmailMessage()
+    msg['Subject'] = subject
+    msg['From'] = Address('Avery Uslaner', addr_spec=USERNAME)
+    msg['To'] = (Address(addr_spec=email) for email in emails)
+    msg.set_content(body_text)
+
+    server.send_message(msg)
+    server.quit()
+
+
+if __name__ == '__main__':
+    emails = ['promopennygather@gmail.com', 'admin@averyuslaner.com']
+    subject = "Test email from Python"
+    body_text = "Python rules them all!"
+    send_email(subject, body_text, emails)
